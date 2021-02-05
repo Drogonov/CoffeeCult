f4f445428c8b9183bb087f88eef4d93e346dd364
Anton Drogonov <vlezko.anton@yandex.ru>
2021-02-05 20:26:48 +0300

    Commit assets

---
+++ CoffeeCult/Model/Cells.swift
@@ -0,0 +1,19 @@
+//
+//  Cells.swift
+//  CoffeeCult
+//
+//  Created by Admin on 29.01.2021.
+//
+
+import Foundation
+
+struct Cells {
+    static let defaultCell = "DefaultCell"
+    static let settingsCell = "SettingsCell"
+    static let profileCell = "ProfileCell"
+    static let photoButtonCell = "PhotoButtonCell"
+    static let textFieldCell = "TextFieldCell"
+    static let actionButtonCell = "ActionButtonCell"
+    static let editButtonCell = "EditButtonCell"
+    static let segmentedControlCell = "SegmentedControlCell"
+}
